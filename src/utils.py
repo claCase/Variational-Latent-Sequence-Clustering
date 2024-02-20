9353d721ed933d3462aea93f418ab423547dcc1c
claudio <claudiocasellato@virgilio.it>
2024-02-20 09:04:53 +0100

    added sin dataset generator

--- src/utils.py
+++ src/utils.py
@@ -51,6 +51,14 @@ def generate_trajectories(
     return trajectories + initial_pos[None, None, :, :]
 
 
+def generate_sin(thetas=(0.1, 1), length=100, samples=50, noise=0.01):
+    thetas = np.asarray(thetas)
+    noise = np.random.normal(size=(len(thetas), samples)) * noise
+    x = np.linspace(np.zeros(len(thetas)), np.ones(len(thetas))*length, length)[:, :, None] * ( thetas[None, :, None] + noise[None, :, :])  #  length x clusters
+    return np.sin(x)  
+
+
+
 if __name__ == '__main__':
     import matplotlib.pyplot as plt 
     diff = [np.eye(2)]*2
